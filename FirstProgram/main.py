08fdbd76d0f5234cea75e91c62128c8809b977f8
hamidehAziz <h_azizmohamadi@yahoo.com>
2019-11-16 11:58:24 -0500

    function with defult arguments is added

--- FirstProgram/main.py
+++ FirstProgram/main.py
@@ -1,1 +1,15 @@
-print("hello world")
+
+
+def my_function(str1, str2):
+    print(str1)
+    print(str2)
+
+
+my_function("hello", "hello again")
+
+
+def print_something(age, name="unknown"):
+    print("my name is:", name, "and my age is:", age)
+
+
+print_something(39)
