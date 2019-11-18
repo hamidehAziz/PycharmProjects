c215403b994e5f73d1dfdf7e1a7ad6b54bf28151
hamidehAziz <h_azizmohamadi@yahoo.com>
2019-11-17 19:11:52 -0500

    return statement

--- FirstProgram/main.py
+++ FirstProgram/main.py
@@ -1,8 +1,10 @@
 
 
-def print_people(*people):
-    for person in people:
-        print("this person is ", person)
+def do_math(n1, n2):
+    return n1 + n2
 
 
-print_people("Dan", "Nick", "Jack", "Smile")
+m1 = do_math(2, 5)
+m2 = do_math(11, 13)
+
+print("first is", m1, "second is", m2)
